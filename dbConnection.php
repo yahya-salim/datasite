4a5610abe0da458a276cb956127baabfac92e82e
yahya-salim <yahya.salim@outlook.fr>
2023-07-23 23:54:58 +0200

    webhost database infos added

--- dbConnection.php
+++ dbConnection.php
@@ -1,9 +1,10 @@
 <?php
+// info de la base 000webhost
 
 $sname="localhost";
-$unmae="root";
-$password="";
-$db_name="datasite";
+$unmae="id21067613_yahya_irl";
+$password="Yahya_irl0";
+$db_name="id21067613_datasite";
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
 if(!$conn) {
     echo "Connection failed !";
