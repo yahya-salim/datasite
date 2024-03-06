f068542553678a27e3d91c8cfede6f75a145addb
yahya-salim <yahya.salim@outlook.fr>
2024-03-06 18:00:44 +0100

    remplacer valeur par getenv("...") dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,9 +1,9 @@
 <?php
 
-$sname="localhost";
-$unmae="root";
-$password="";
-$db_name="datasite";
+$sname=getenv("HOSTNAME");
+$unmae=getenv("USERNAME");
+$password=getenv("PSWD");
+$db_name=getenv("DBNAME");
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
 if(!$conn) {
     echo "Connection failed !";
